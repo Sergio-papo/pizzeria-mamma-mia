871cb04520cf0fa618fae6ad3dbb732db51a0ade
Sergio Inostroza C. <sinostroza@gud.cl>
2026-03-29 21:28:49 -0300

    subir proyecto actualizado

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './Navbar'
+import Home from './Home'
+import Footer from './Footer'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
